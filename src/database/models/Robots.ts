a23617eeca7a542a08a4b519161b5cdb6c7142d5
Víctor Merino <victor.cornella.86@gmail.com>
2022-11-05 15:34:05 +0100

    Add database schema document of robots collections and fix eslint rule

---
+++ src/database/models/Robots.ts
@@ -0,0 +1,34 @@
+import { model, Schema } from "mongoose";
+
+const robotSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  characteristics: {
+    speed: {
+      type: Number,
+      min: 0,
+      max: 10,
+      required: true,
+    },
+    stamina: {
+      type: Number,
+      min: 0,
+      max: 10,
+      required: true,
+    },
+    creationDate: {
+      type: Date,
+      required: true,
+    },
+  },
+});
+
+const Robot = model("robots", robotSchema, "robots");
+
+export default Robot;
